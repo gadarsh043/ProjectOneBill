233a6c913730984e2e238d389fa6e73320c37303
Chaitanya Jain <chaitanyajain@Chaitanyas-MacBook-Pro.local>
2021-03-02 14:39:01 +0530

    zero

---
+++ src/main/java/com/example/ProjectOneBill/ProjectOneBillApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectOneBill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOneBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOneBillApplication.class, args);
+	}
+
+}
