54a63f2e972d8e493d6f8d948bc2f99873108f05
Chaitanya Jain <chaitanyajain@Chaitanyas-MacBook-Pro.local>
2021-03-05 16:06:29 +0530

    zuulWorking

--- src/main/java/com/example/ProjectOneBill/ProjectOneBillApplication.java
+++ src/main/java/com/example/ProjectOneBill/ProjectOneBillApplication.java
@@ -2,9 +2,11 @@ package com.example.ProjectOneBill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ProjectOneBillApplication {
 
